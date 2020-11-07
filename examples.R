397a202592c49970e457f755f9492f45cb462439
Gael <Gael@WL20-0067.corp.pasteur.fr>
2020-11-07 15:49:43 +0100

    tempo

--- examples.R
+++ examples.R
@@ -173,14 +173,14 @@ l1 <- list(L1 = 1:3, L2 = letters[1:3]) # list
 fac1 <- factor(rep(letters[4:6], c(4:6))) # factor
 tab1 <- table(fac1) # 1D table
 tab2 <- table(fac1, fac1) # 2D table
-exp1 <- expression("a") # expression
+exp1 <- expression("a") # object of class "expression", mode "expression" & type "expression"
 name1 <- substitute(exp1) # object of class "name", mode "name" & type "symbol"
-fun1 <- mean # function type "closure"
-fun2 <- sum # function primitive type "builtin"
-fun3 <- get("<-") # function primitive type "special"
+fun1 <- mean # closure function of class "function", mode "function" & type "closure"
+fun2 <- sum # primitive function of class "function", mode "function" & type "builtin"
+fun3 <- get("<-") # primitive function of class "function", mode "function" &  type "special"
 env1 <- new.env() # environment
 s4 <- show # S4 object
-
+call1 <- call("call1") # object of class "call", mode "call" & type "language"
 
 ### Datasets info
 vec1 # vector of integers
@@ -195,13 +195,15 @@ l1 # list
 fac1 # factor
 tab1 # 1D table
 tab2 # 2D table
-exp1 # expression
+exp1 # object of class "expression", mode "expression" & type "expression"
 name1 # object of class "name", mode "name" & type "symbol"
-fun1 # function type "closure"
-fun2 # function primitive type "builtin"
-fun3 # function primitive type "special"
+fun1 # closure function of class "function", mode "function" & type "closure"
+fun2 # primitive function of class "function", mode "function" & type "builtin"
+fun3 # primitive function of class "function", mode "function" &  type "special"
 env1 # environment
 s4 # S4 object
+call1 # object of class "call", mode "call" &  type "language"
+
 
 ### Simple example
 fun_info(data = vec1) # vector of integers
@@ -216,13 +218,15 @@ fun_info(data = l1) # list
 fun_info(data = fac1) # factor
 fun_info(data = tab1) # 1D table
 fun_info(data = tab2) # 2D table
-fun_info(data = exp1) # expression
+fun_info(data = exp1) # object of class "expression", mode "expression" & type "expression"
 fun_info(data = name1) # object of class "name", mode "name" & type "symbol"
-fun_info(data = fun1) # function type "closure"
-fun_info(data = fun2) # function primitive type "builtin"
-fun_info(data = fun3) # function primitive type "special"
+fun_info(data = fun1) # closure function of class "function", mode "function" & type "closure"
+fun_info(data = fun2) # primitive function of class "function", mode "function" & type "builtin"
+fun_info(data = fun3) # primitive function of class "function", mode "function" &  type "special"
 fun_info(data = env1) # environment
 fun_info(data = s4) # S4 object
+fun_info(data = call1) # object of class "call", mode "call" &  type "language"
+
 
 
 ### All the arguments
